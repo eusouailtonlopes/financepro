87dfdf32687eb9fe919f1ef85620b39fb37c6930
eusouailtonlopes <lopesailtonsilva@gmail.com>
2025-07-29 08:43:26 -0400

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,7 @@
-// placeholder content for tailwind.config.js
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
